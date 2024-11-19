06de4506b9a41a463e76842c31c39a73d584bb30
maxxxkam <muslimovmaxkam@gmail.com>
2024-11-19 19:09:48 +0500

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './Hero.module.scss';
+
+const Hero = () => {
+  return (
+    <>
+      <secton className={s.hero}>
+        <div className={s.box}>
+
+        </div>
+      </secton>
+    </>
+  );
+};
+
+export default Hero;
