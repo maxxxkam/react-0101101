6264ac3750b11b4900c9730235eb8d6589e027e8
maxxxkam <muslimovmaxkam@gmail.com>
2024-11-23 16:45:04 +0500

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,10 @@ import Hero from '../components/Hero/Hero';
 import Brand from '../components/brands/Brand';
 import SectionTitle from '../components/SectionTitle/SectionTitle';
 import Category from '../components/category/Category';
+import Browse from '../components/browse/Browse';
+import Happy from '../components/Happy/Happy';
+import Stay from '../components/stay/Stay';
+import Footer from '../components/footer/Footer';
 
 const Home = () => {
   return (
@@ -12,6 +16,10 @@ const Home = () => {
       <Hero />
       <Brand/>
       <Category/>
+      <Browse/>
+      <Happy/>
+      <Stay/>
+      <Footer/>
     </>
   );
 };
