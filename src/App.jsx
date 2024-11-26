d079428d6a9324db5b1197ef514cc157f7ba77fe
maxxxkam <muslimovmaxkam@gmail.com>
2024-11-26 19:00:27 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import './App.css'
 import { Route, Router, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Footer from './components/footer/Footer'
+import Header from './components/Header/Header'
+import Shop from './pages/Shop'
 
 const App = () => {
   return (
     <>
+     <Header />
     <Routes>
     <Route path='/' element={<Home/>} />
+    <Route path='/shop' element={<Shop/>} />
     </Routes>
+    <Footer/>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import Header from '../components/Header/Header';
 import Hero from '../components/Hero/Hero';
 import Brand from '../components/brands/Brand';
-import SectionTitle from '../components/SectionTitle/SectionTitle';
 import Category from '../components/category/Category';
 import Browse from '../components/browse/Browse';
 import Happy from '../components/Happy/Happy';
 import Stay from '../components/stay/Stay';
-import Footer from '../components/footer/Footer';
 
 const Home = () => {
   return (
     <>
-      <Header />
+     
       <Hero />
       <Brand/>
       <Category/>
       <Browse/>
       <Happy/>
       <Stay/>
-      <Footer/>
+      
     </>
   );
 };
