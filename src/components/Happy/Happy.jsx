9496a6a235de41134321a99cd717d3f59316d394
maxxxkam <muslimovmaxkam@gmail.com>
2024-11-27 16:46:12 +0500

    Add files via upload

--- src/components/Happy/Happy.jsx
+++ src/components/Happy/Happy.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./Happy.module.scss";
 import SectionTitle from "../SectionTitle/SectionTitle";
+import Card2 from "../Card2/Card2";
 
 const Happy = () => {
   return (
@@ -16,28 +17,10 @@ const Happy = () => {
               </div>
             </div>
             <div className={s.cards}>
-              <div className={s.card}>
-                <img src="/Happy-svg1.svg" alt="" />
-                <div>
-                <b>Sarah M.<img src="/Happy-svg2.svg" alt="" /></b>
-                <p>"I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”</p>
-                </div>
-              </div>
-              <div className={s.card}>
-                <img src="/Happy-svg1.svg" alt="" />
-                <div>
-                <b>Alex K.<img src="/Happy-svg2.svg" alt="" /></b>
-                <p>
-                "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.”</p>
-                </div>
-              </div>
-              <div className={s.card}>
-                <img src="/Happy-svg1.svg" alt="" />
-                <div>
-                <b>James L.<img src="/Happy-svg2.svg" alt="" /></b>
-                <p>"As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”</p>
-                </div>
-              </div>
+             <Card2 name='Sarah M.' about='Im blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece Ive bought has exceeded my expectations.'/>
+             <Card2 name='Alex K.' about='"Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.”'/>
+             <Card2 name='James L.' about="As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends"/>
+              
             </div>
           </div>
         </div>
